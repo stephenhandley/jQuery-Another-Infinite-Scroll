65c411f19d79e7d0391c1c5b050306319d16d5a5
Stephen Handley <stephen@imac.local>
2011-03-08 17:58:44 -0800

    don't needlessly calculate scroll height if a request is pending..rename option

--- jquery.anotherinfinitescroll.js
+++ jquery.anotherinfinitescroll.js
@@ -2,7 +2,7 @@
   $.fn.infinitescroll = function(options) {
     return $(this).each(function() {      
       var settings = $.extend({
-          triggerAt: 400,
+          lookahead: 400,
           container: $(document)
         }, options);
         
@@ -25,19 +25,14 @@
       }
 
       $(window).scroll(function(e) {
-        var distanceToBottom = $(document).height() - (settings.container.scrollTop() || $(settings.container.get().ownerDocument.body).scrollTop()) - $(window).height();
+        if (req && req.readyState < 4 && req.readyState > 0) {
+          return;
+        }
+
+        var distanceToBottom = $(document).height() - (settings.container.scrollTop() || $(settings.container.get().ownerDocument.body).scrollTop()) - $(window).height();        
         
-        if (distanceToBottom < settings.triggerAt) {
-          if (req && req.readyState < 4 && req.readyState > 0) {
-            return;
-          }
-          
-          req = $.get(
-            nextLink.attr('href'), 
-            {}, 
-            settings.callback,
-            'json'
-          );
+        if (distanceToBottom < settings.lookahead) {  
+          req = $.get(nextLink.attr('href'), {}, settings.callback, 'json');
         }
       });
     });
